a185585356b1098f9bfe54a7cbc81cb9bb70838f
Juha Leinonen <jsilvanus@gmail.com>
2016-10-17 01:49:14 +0300

    Settings and loacle * Adding settings.yml * Add server.en.yml * 'Fix i18n * Added missing lines

--- plugin.rb
+++ plugin.rb
@@ -39,10 +39,10 @@ after_initialize do
       def create(user_id, handle)
         ensureStaff user_id
 
-        raise StandardError.new "citestore.create.nohandle" if handle.blank?
+        raise StandardError.new "citestore.missing_handle" if handle.blank?
 
         storage = all(user_id)
-        raise StandardError.new "citestore.create.already_exists" if storage.include?(handle)
+        raise StandardError.new "citestore.already_exists" if storage.include?(handle)
 
         storage << handle
         PluginStore.set(PLUGIN_NAME, STORE_NAME, storage)
@@ -53,8 +53,8 @@ after_initialize do
       def delete(user_id, handle)
         ensureStaff used_id
 
-        raise StandardError.new "citestore.delete.nohandle" if handle.blank?
-        raise StandardError.new "citestore.delete.nostorage" if exists(handle) == nil
+        raise StandardError.new "citestore.missing_handle" if handle.blank?
+        raise StandardError.new "citestore.missing_storage" if exists(handle) == nil
 
         storage = all(user_id)
         storage.pop => handle
@@ -66,26 +66,26 @@ after_initialize do
 
       # Get locus from storage
       def whole(handle)
-        raise StandardError.new "citestore.whole.nohandle" if handle.blank?
-        raise StandardError.new "citestore.whole.nostore" if exists(handle) == nil
+        raise StandardError.new "citestore.missing_handle" if handle.blank?
+        raise StandardError.new "citestore.missing_store" if exists(handle) == nil
         storage = PluginStore.get(PLUGIN_NAME, handle)
 
         storage
       end
 
       def has(handle, locus)
-        raise StandardError.new "citestore.has.nohandle" if handle.blank?
-        raise StandardError.new "citestore.has.nolocus" if locus.blank?
+        raise StandardError.new "citestore.missing_handle" if handle.blank?
+        raise StandardError.new "citestore.missing_locus" if locus.blank?
         storage = PluginStore.get(PLUGIN_NAME, handle)
         storage.has_key?(locus)
       end
 
       def get(handle, locus)
-        raise StandardError.new "citestore.get.nohandle" if handle.blank?
-        raise StandardError.new "citestore.get.nostore" if exists(handle) == nil
+        raise StandardError.new "citestore.missing_handle" if handle.blank?
+        raise StandardError.new "citestore.missing_store" if exists(handle) == nil
 
         storage = PluginStore.get(PLUGIN_NAME, handle)
-        raise StandardError.new "citestore.get.nolocus" if storage.has_key?(locus) == nil
+        raise StandardError.new "citestore.missing_locus" if storage.has_key?(locus) == nil
 
         record = storage[locus]
         record
@@ -95,13 +95,13 @@ after_initialize do
       def add(user_id, handle, locus, contents, force = nil)
         ensureStaff user_id
 
-        raise StandardError.new "citestore.add.nohandle" if handle.blank?
-        raise StandardError.new "citestore.add.nolocus" if locus.blank?
-        raise StandardError.new "citestore.add.nocontents" if contents.blank?
+        raise StandardError.new "citestore.missing_handle" if handle.blank?
+        raise StandardError.new "citestore.missing_locus" if locus.blank?
+        raise StandardError.new "citestore.missing_content" if contents.blank?
 
         if exists(handle) == nil
           if force == nil
-            raise StandardError.new "citestore.add.nostore"
+            raise StandardError.new "citestore.missing_store"
           end
 
           create(user_id, handle)
